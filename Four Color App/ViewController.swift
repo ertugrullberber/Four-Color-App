6922d1596faf3f27f28bc03889d33cffa807f872
ertugrullberber <ertugrulberber3461@gmail.com>
2022-10-12 13:58:43 +0300

    first commit

---
+++ Four Color App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Four Color App
+//
+//  Created by Ertugrul Berber on 12.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var pkk: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
